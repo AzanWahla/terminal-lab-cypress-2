8b08eed26f6327aecc32b3b6c01aa82b15954b76
Azan Wahla <114916513+AzanWahla@users.noreply.github.com>
2026-05-19 04:22:45 +0500

    Update cypress/support/e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -1,1 +1,14 @@
 import "./commands";
+
+Cypress.on("uncaught:exception", (err) => {
+  if (err.message.includes("backtrace")) {
+    return false;
+  }
+});
+
+beforeEach(() => {
+  cy.intercept("POST", "https://events.backtrace.io/**", {
+    statusCode: 204,
+    body: {}
+  });
+});
